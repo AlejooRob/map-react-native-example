9dc81d27c0378d11912f4e4a37dca3f5a10d28f8
Alejo <alejolink001@gmail.com>
2022-03-08 20:56:55 -0500

    add hook and fixes

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Map from '../components/Map';
 
 export default function MapScreen() {
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View style={{ flex: 1}}>
+      <Map />
     </View>
   )
 }
